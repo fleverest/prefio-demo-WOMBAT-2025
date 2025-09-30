0584813ce35422459a2fa0f17c923891e6f19370
Floyd Everest <floyd.everest@monash.edu>
2025-09-30 16:06:50 +1000

    Added election results, fixed small error in election results script

--- election_results.R
+++ election_results.R
@@ -6,7 +6,7 @@ election_data <- read_csv("voter_app/data/responses.csv")
 
 election_votes <- election_data |>
   long_preferences(vote,
-                   id_cols = device_hash,
+                   id_cols = c(device_hash, timestamp),
                    rank_col = rank,
                    item_col = item)
 
